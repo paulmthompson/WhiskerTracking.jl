76e5f7b759ace1c598f59ff0362c7f5a0e4010e1
Paul Thompson <pmt10@case.edu>
2019-03-23 08:57:27 -0400

    fix 0.6 deprecations

--- src/WhiskerTracking.jl
+++ src/WhiskerTracking.jl
@@ -3,6 +3,11 @@ module WhiskerTracking
 
 using Gtk.ShortNames, Cairo, Images, StatsBase, ImageFiltering, MAT, JLD, Interpolations, Distances, DSP
 
+if VERSION > v"0.7-"
+    using SharedArrays, Libdl
+    const Void = Nothing
+end
+
 include("types.jl")
 include("gui.jl")
 include("janelia_tracker.jl")
