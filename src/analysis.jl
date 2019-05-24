f43bcc8fd198d1088e7f2165b1284e711bc200c0
Paul Thompson <pmt10@case.edu>
2019-05-24 14:41:48 -0400

    position position locator

--- src/analysis.jl
+++ src/analysis.jl
@@ -296,3 +296,89 @@ function outlier_removal_twosided(cov1,min_ex,max_ex)
     end
     nothing
 end
+
+#Finds the number of unique pole positions
+
+function get_p_pos(p)
+
+    pos_labels=zeros(Int64,size(p,1))
+
+    num_labels=0
+    pos_x=Array{Float64}(0)
+    pos_y=Array{Float64}(0)
+
+    new_thres=50.0
+
+    for i=1:size(p,1)
+        if !isnan(p[i,1])
+
+            new_label_flag=true
+
+            for j=1:num_labels
+
+                if sqrt((p[i,1]-pos_x[j])^2+(p[i,2]-pos_y[j])^2) < new_thres
+                    new_label_flag=false
+                    pos_labels[i] = j
+                    break
+                end
+            end
+
+            if new_label_flag
+                num_labels += 1
+                push!(pos_x,p[i,1])
+                push!(pos_y,p[i,2])
+                pos_labels[i]=num_labels
+            end
+        else
+
+            pos_labels[i]=0
+
+        end
+    end
+
+    for i=1:num_labels
+        if length(find(pos_labels.==i))<1000
+            pos_labels[pos_labels.==i] = 0
+        end
+    end
+
+    for i=unique(pos_labels)
+
+        max_in_a_row=0
+        temp_in_a_row=0
+        in_a_row=false
+
+        for j=1:length(pos_labels)-1
+
+            if pos_labels[j]==i
+
+                if in_a_row
+                    temp_in_a_row += 1
+
+                else
+                    temp_in_a_row = 1
+                    in_a_row = true
+                end
+
+            elseif in_a_row
+
+                in_a_row = false
+                if temp_in_a_row > max_in_a_row
+                    max_in_a_row = temp_in_a_row
+                end
+
+            end
+        end
+
+        if max_in_a_row < 500
+
+            pos_labels[pos_labels.==i] = 0
+
+        end
+    end
+
+    non_zero_labels=unique(pos_labels)
+    non_zero_labels=non_zero_labels[non_zero_labels.!=0]
+
+    (pos_labels, pos_x[non_zero_labels], pos_y[non_zero_labels])
+end
