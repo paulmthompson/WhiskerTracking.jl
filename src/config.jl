536e020a525d5134745b49d00077d9d76db16050
Paul Thompson <pmt11@dhe.duke.edu>
2019-09-03 10:09:01 -0400

    allow homedir path to be a variable

--- src/config.jl
+++ src/config.jl
@@ -1,6 +1,6 @@
 if is_unix()
-    const libwhisk_path = "/home/wanglab/Programs/whisk/build/libwhisk.so"
-    jt_parameters = "/home/wanglab/Programs/whisk/build/default.parameters"
+    const libwhisk_path = string(homedir(),"/home/wanglab/Programs/whisk/build/libwhisk.so")
+    jt_parameters = string(homedir(),"/home/wanglab/Programs/whisk/build/default.parameters")
 else
     const libwhisk_path = "C:\\Program Files\\WhiskerTracking\\lib\\whisk.dll"
     jt_parameters = "C:\\Program Files\\WhiskerTracking\\default.parameters"
@@ -9,8 +9,8 @@ end
 libwhisk = Libdl.dlopen(libwhisk_path)
 
 if is_unix()
-    const ffmpeg_path = "/home/wanglab/Programs/ffmpeg/ffmpeg"
-    const ffprobe_path = "/home/wanglab/Programs/ffmpeg/ffprobe"
+    const ffmpeg_path = string(homedir(),"/home/wanglab/Programs/ffmpeg/ffmpeg")
+    const ffprobe_path = string(homedir(),"/home/wanglab/Programs/ffmpeg/ffprobe")
 else
     const ffmpeg_path = string(homedir(),"\\Documents\\ffmpeg\\bin\\ffmpeg")
     const ffprobe_path = string(homedir(),"\\Documents\\ffmpeg\\bin\\ffprobe")
