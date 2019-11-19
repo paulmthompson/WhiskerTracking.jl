98e71dd9728380b5981af4d5f788375ab70dc0c9
Paul Thompson <pmt11@duke.edu>
2019-11-19 17:24:12 -0500

    better contact method organization

--- src/gui.jl
+++ src/gui.jl
@@ -425,28 +425,6 @@ end
 Touch Functions
 =#
 
-function touch_override_cb(w::Ptr,user_data::Tuple{Tracker_Handles,Int64})
-
-    han, contact = user_data
-
-    #Check if this frame is already in the contact list
-    if isempty(find(han.touch_frames_i.==han.displayed_frame))
-
-        push!(han.touch_frames_i,han.displayed_frame)
-        push!(han.touch_frames,contact)
-
-        #Do we need to sort these here?
-
-    else #Change existing entry
-
-        ind=find(han.touch_frames_i.==han.displayed_frame)[1]
-        han.touch_frames[ind] = contact
-    end
-
-    draw_touch(han)
-
-    nothing
-end
 
 function detect_touch(han)
 
@@ -802,50 +780,6 @@ function load_cb(w::Ptr,user_data::Tuple{Tracker_Handles})
     nothing
 end
 
-function save_contact_cb(w::Ptr,user_data::Tuple{Tracker_Handles})
-
-    han, = user_data
-
-    filepath = save_dialog("Save Contact Data",han.win)
-
-    if filepath != ""
-        if filepath[end-3:end]==".mat"
-        else
-            filepath=string(filepath,".mat")
-        end
-
-        file=matopen(filepath,"w")
-        write(file,"Touch_Inds",han.touch_frames_i)
-        write(file,"Touch",convert(Array{Int64,1},han.touch_frames))
-        close(file)
-    end
-
-    nothing
-end
-
-function load_contact_cb(w::Ptr,user_data::Tuple{Tracker_Handles})
-
-    han, = user_data
-
-    filepath = open_dialog("Load Contact Data",han.win)
-
-    if filepath != ""
-        file = matopen(filepath,"r")
-        if MAT.exists(file,"Touch_Inds")
-            touch_inds = read(file,"Touch_Inds")
-            han.touch_frames_i = touch_inds
-        end
-        if MAT.exists(file,"Touch")
-            touch = read(file,"Touch")
-            han.touch_frames = convert(BitArray{1},touch)
-        end
-        close(file)
-    end
-
-    nothing
-end
-
-
 function advance_slider_cb(w::Ptr,param_tuple,user_data::Tuple{Tracker_Handles})
 
     han, = user_data
@@ -862,8 +796,6 @@ function advance_slider_cb(w::Ptr,param_tuple,user_data::Tuple{Tracker_Handles})
 
     nothing
 end
-
-
 
 function frame_select(w::Ptr,user_data::Tuple{Tracker_Handles})
 
