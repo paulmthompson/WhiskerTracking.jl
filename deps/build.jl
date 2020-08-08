3e872a1c3e6e6c0b003da8ba8d2bd1e13809dd13
Paul Thompson <pmt10@case.edu>
2020-08-08 18:17:52 -0400

    fix removing step

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,15 @@
 
 
-if is_linux()
+if Sys.islinux()
     run(`wget https://www.dropbox.com/s/1dr7g8x270xdrup/whisk-1.1.0d-64bit-Linux.tar.gz`)
     run(`tar -xzf whisk-1.1.0d-64bit-Linux.tar.gz`)
-    run(`rm whisk-1.1.0d-64bit-Linux.tar.gz`)
+    rm("whisk-1.1.0d-64bit-Linux.tar.gz")
     run(`mv whisk-1.1.0d-64bit-Linux whisk`)
-elseif is_windows()
+elseif Sys.iswindows()
     run(`curl.exe --url https://www.dropbox.com/s/0dwksb5pihauvz8/whisker.zip?dl=0 -o whisk.zip -L`)
     run(`powershell.exe -nologo -noprofile -command "Expand-Archive -Literalpath ./whisk.zip"`)
-    run(`del whisk.zip`)
-elseif is_osx()
+    rm("whisk.zip")
+elseif Sys.isosx()
     println("No Mac support!")
 else
     println("Operating system not recognized.")
