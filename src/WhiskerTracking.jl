a0d828f8bcaf91f17e47755dd50b43a958fe3d47
Paul Thompson <pmt10@case.edu>
2021-02-24 14:22:28 -0500

    add polynomials as dependancy

--- src/WhiskerTracking.jl
+++ src/WhiskerTracking.jl
@@ -11,7 +11,7 @@ using CuArrays, CuArrays.CUFFT, CUDAnative, Knet
 using PyCall, ScikitLearn
 
 using Gtk.ShortNames, Cairo, Images, ImageFiltering, MAT, JLD2, Interpolations, DSP,
-FFTW, IterTools, FFMPEG, StackedHourglass
+FFTW, IterTools, FFMPEG, StackedHourglass, Polynomials
 
 @sk_import ensemble: RandomForestClassifier
 
