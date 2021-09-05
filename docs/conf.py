b66c52f052643ea54a0f15b5cb1bed872114e79c
Paul Thompson <pmt10@case.edu>
2021-09-05 13:07:55 -0400

    updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,4 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = [
-    'css/custom.css',
-]
+html_css_files = ["css/custom.css"]
