1a34ba141bea5bd7ff2f8811e2043a2ea2c9871c
Paul Thompson <pmt10@case.edu>
2021-09-06 18:42:19 -0400

    updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -60,9 +60,6 @@ master_doc = 'index'
 import sphinx_rtd_theme
 html_theme = 'alabaster'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {
-    'sidebarwidth' : 300
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
