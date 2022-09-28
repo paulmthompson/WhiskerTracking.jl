77996a67490a00d0adc5c99a94fe26423af6eecb
Paul Thompson <pmt10@case.edu>
2022-09-28 11:45:51 -0400

    remove all Gtk stuff from this package

--- src/WhiskerTracking.jl
+++ src/WhiskerTracking.jl
@@ -38,37 +38,4 @@ include("analysis/forces.jl")
 include("analysis/touch.jl")
 include("analysis/math.jl")
 
-module GUI
-    using ..WhiskerTracking
-    using Gtk.ShortNames, Cairo, FFMPEG, StackedHourglass
-    import ..WhiskerTracking: Whisker1, Tracker, Tracked_Whisker, classifier, NeuralNetwork,
-        Manual_Class
-    #export WhiskerTracking
-    include("gui/types.jl")
-    include("gui/save_load.jl")
-    include("gui/gui.jl")
-    include("gui/manual.jl")
-    include("gui/analog.jl")
-    include("gui/whisker_pad.jl")
-    include("gui/discrete.jl")
-    include("gui/mask.jl")
-    include("gui/pole.jl")
-    include("gui/view.jl")
-    include("gui/tracing.jl")
-    include("gui/image.jl")
-    include("gui/janelia.jl")
-    include("gui/export.jl")
-    include("gui/contact.jl")
-    include("gui/deeplearning.jl")
-    include("gui/zoom.jl")
-    include("gui/whisker_table.jl")
-
-    include("drawing_tools/draw.jl")
-    include("drawing_tools/erase.jl")
-    include("drawing_tools/shapes.jl")
-end
-using .GUI
-#include("precompile.jl")
-#_precompile()
-
 end
