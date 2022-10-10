a42bd9e93dc78cf344f865c71cc2e908a08be408
Paul Thompson <pmt10@case.edu>
2022-10-10 16:46:26 -0400

    move mask application method here from gtk

--- src/mask.jl
+++ src/mask.jl
@@ -52,6 +52,9 @@ function mask_tracked_whisker(w_x,w_y,wt::WhiskerTracking.Tracker,thres=30.0)
         end
     end
 
+    #=
+    This is inaccurate because it is calculating the angle from just two points.
+    =#
     theta = atan(w_y[out_ind] - w_y[out_ind + 1],w_x[out_ind] - w_x[out_ind + 1])
 
     x = w_x[out_ind]
@@ -76,4 +79,56 @@ function mask_tracked_whisker(w_x,w_y,wt::WhiskerTracking.Tracker,thres=30.0)
     end
 
     (out_ind,x_0,y_0,x,y)
+end
+
+function apply_mask(whiskers::Array{Whisker1,1},mask::BitArray{2},min_length::Int64)
+
+    remove_whiskers=Array{Int64,1}()
+
+    for i=1:length(whiskers)
+        delete_points=Array{Int64,1}()
+
+        #Start at the tip and work our way back to the follicle.
+        #If the mask hits something, we should probably delete all points following
+        for j=1:length(whiskers[i].x)
+            x_ind = round(Int64,whiskers[i].y[j])
+            y_ind = round(Int64,whiskers[i].x[j])
+
+            if x_ind<1
+                x_ind=1
+            elseif x_ind>size(mask,1)
+                x_ind=size(mask,1)
+            end
+
+            if y_ind<1
+                y_ind=1
+            elseif y_ind>size(mask,2)
+                y_ind=size(mask,2)
+            end
+
+            if mask[x_ind,y_ind]
+                for k=j:length(whiskers[i].x)
+                    push!(delete_points,k)
+                end
+                break
+            end
+        end
+
+        deleteat!(whiskers[i].x,delete_points)
+        deleteat!(whiskers[i].y,delete_points)
+        deleteat!(whiskers[i].thick,delete_points)
+        deleteat!(whiskers[i].scores,delete_points)
+        whiskers[i].len = length(whiskers[i].x)
+
+        #Sometimes whiskers are detected in mask of reasonable length, so they are completely deleted
+        #In this step and will mess up later processing, so we should delete them after a length check
+        if whiskers[i].len < min_length
+            push!(remove_whiskers,i)
+        end
+
+    end
+
+    deleteat!(whiskers,remove_whiskers)
+
+    nothing
 end
