afaa16886140f9f3b81483b17309784dc1cd8f19
Paul Thompson <pmt10@case.edu>
2022-10-14 16:05:58 -0400

    remove unnecessary inputs from mask method

--- src/mask.jl
+++ src/mask.jl
@@ -112,17 +112,17 @@ function mask_tracked_whisker(w_x,w_y,mask,extended_mask,thres=30.0)
 
     (theta,out_ind,xx,yy) = find_angle_clipping(w_x,w_y,extended_mask)
 
-    mask_tracked_whisker(w_x,w_y,theta,out_ind,xx,yy,mask,thres)
+    mask_tracked_whisker(w_x,w_y,theta,out_ind,mask,thres)
 end
 
 function mask_tracked_whisker(w_x,w_y,wt::WhiskerTracking.Tracker,thres=30.0)
 
     (theta,out_ind,xx,yy) = find_angle_clipping(w_x,w_y,wt)
 
-    mask_tracked_whisker(w_x,w_y,theta,out_ind,xx,yy,wt.mask,thres)
+    mask_tracked_whisker(w_x,w_y,theta,out_ind,wt.mask,thres)
 end
 
-function mask_tracked_whisker(w_x,w_y,theta,out_ind,xx,yy,mask,thres=30.0)
+function mask_tracked_whisker(w_x,w_y,theta,out_ind,mask,thres=30.0)
 
     x = w_x[out_ind]
     y = w_y[out_ind]
