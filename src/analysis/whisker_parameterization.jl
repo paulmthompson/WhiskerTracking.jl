b9310f390cd35eb55840b6beaf39b8da397b2609
Paul Thompson <pmt10@case.edu>
2023-01-09 09:39:21 -0500

    added function which can find the point on a whisker which is closest to some arbitrary point not on the whisker and interpolates to find find exact location in the middle of line segment

--- src/analysis/whisker_parameterization.jl
+++ src/analysis/whisker_parameterization.jl
@@ -1,8 +1,8 @@
 
-
+dist(x1,y1,x2,y2) = sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) *  (x2 - x1))
 
 function distance_along(x::Array{T,1},y::Array{T,1},i::Int64) where T
-    sqrt((y[i]-y[i-1])^2+(x[i]-x[i-1])^2)
+    dist(x[i-1],y[i-1],x[i],y[i])
 end
 
 function total_length(x::Array{T,1},y::Array{T,1}) where T
@@ -57,4 +57,63 @@ function get_ind_at_dist_exact(x,y,thres)
         s0 = s1
     end
     (x_out,y_out)
+end
+
+
+#=
+Given a whisker parameterized by points (x_1,y_1), (x_2,y_2), ... (x_n, y_n), 
+This finds the nearest interpolated point, (x_i,y_i) that is closest to 
+the point not on the whisker (x_p,y_p)
+
+https://math.stackexchange.com/questions/2193720/find-a-point-on-a-line-segment-which-is-the-closest-to-other-point-not-on-the-li
+=#
+
+function find_nearest_on_whisker(x::Array{T,1},y::Array{T,1},x_p,y_p) where T
+
+    x_closest = x[1]
+    y_closest = y[1]
+
+    closest_dist = dist(x[1],y[1],x_p,y_p)
+
+    A_dist = closest_dist
+
+    for i=2:length(x)
+
+        Ax = x[i - 1]
+        Ay = y[i - 1]
+        Bx = x[i]
+        By = y[i]
+        B_dist = dist(Bx,By,x_p,y_p)
+
+        vx = Bx - Ax 
+        vy = By - Ay 
+
+        ux = Ax - x_p 
+        uy = Ay - y_p 
+
+        t = -1 * (vx * ux + vy * uy) / (vx * vx + vy * vy)
+
+        if (t > 0) & (t < 1)
+            t_x = (1-t) * Ax + t * Bx
+            t_y = (1-t) * Ay + t * By
+
+            min_dist = dist(t_x,t_y,x_p,y_p)
+
+            if (min_dist < closest_dist)
+                closest_dist = min_dist 
+                x_closest = t_x 
+                y_closest = t_y
+            end
+        else
+            if (B_dist < closest_dist) 
+                closest_dist = B_dist 
+                x_closest = Bx 
+                y_closest = By
+            end
+        end
+
+        A_dist = B_dist
+    end
+
+    (x_closest,y_closest)
 end
